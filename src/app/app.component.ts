9b7996f90c0fccfd2130895e159318c0776db1c7
akshaysankeysolutions <akshay.p1@sankeysolutions.com>
2024-03-17 22:23:16 +0530

    responsiveness added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,64 +7,92 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  // supplierDetails:{ [key: string]: string }={
-  //   "Company Name":"XYZ SYSTEMS LTD",
-  //   "Company Address":"Thane",
-  //   "Pkg Person Contact Name":"Sanket Pawar",
-  //   "Email":"Sanket.Pawar@gmail.com",
-  //   "Vendor Code":"R12345",
-  //   "State, Country":"MAHARASHTRA, INDIA",
-  //   "Tel No":"Q5",
-  //   "Fax No":"Q5"
-  // }
-
-  supplierDetails:any={
-    companyName:"XYZ SYSTEMS LTD",
-    companyAddress:"Thane",
-    pkgPersonName:"Sanket Pawar",
-    email:"Sanket.Pawar@gmail.com",
-    vendorCode:"R12345",
-    stateCountry:"MAHARASHTRA, INDIA",
-    telNo:"Q5",
-    faxNo:"Q5"
-  }
-
-
-  packageStatus:{[key:string]:string}={
-    "Approval Type":"Final",
-    "Status":"Recommended",
-    "Date of Approval":"28-04-2024"
-  }
-
-  programPartInfo:any={
-    receiverLocation: "ABC to VGH to TYX",
-    partDimension:[120,130,200],
-    partNo:12345678909,
-    volume:75000,
-    part_desc:"R_TRW_1234",
-    partPerVehicle:1,
-    partWeight:0.38,
-    modelsApplicable:"Q5"
-  }
-
-  visual_reference:{[key:string]:string}={
-    "3.1 - Part":"../assets/defaultImg.png",
-    "3.2 - Part":"../assets/defaultImg.png",
-    "3.3 - Part":"../assets/defaultImg.png"
-  }
-
-    Specifications=["Density (Qty of parts)","Packaging Strategy","Carton Code","Carton Type(Design)","Packaging Material Type","Securement / Closure Type","Total Loaded Wt. (kg - Full)","Outside Dimension (L X W X H) (mm)","Dunnage & Description","Labels (Quantity / Location)","Container Ownership"];
-
-    primaryPackaging=["-","-","-","-","-","-","-","-","-","-","-"];
-
-    secondaryPackaging=[5,"Returnable","NA","Tree Bin Bag","Tree Bin Bag","Remarks",2.19,[600,400,180],["Yes","Xyz"],[2,"Adjacent Side"],"XYZ"];
-
-    approvalData={
-      data:[
+  data:any={
+    supplierDetails: {
+      companyName: "XYZ SYSTEMS LTD",
+      companyAddress: "Thane",
+      pkgPersonName: "Sanket Pawar",
+      email: "Sanket.Pawar@gmail.com",
+      vendorCode: "R12345",
+      stateCountry: "MAHARASHTRA, INDIA",
+      telNo: "Q5",
+      faxNo: "Q5"
+    },
+    packageStatus:{
+      "Approval Type":"Final",
+      "Status":"Recommended",
+      "Date of Approval":"28-04-2024"
+    },
+    programPartInfo:{
+      receiverLocation: "ABC to VGH to TYX",
+      partDimension:[120,130,200],
+      partNo:12345678909,
+      volume:75000,
+      part_desc:"R_TRW_1234",
+      partPerVehicle:1,
+      partWeight:0.38,
+      modelsApplicable:"Q5"
+    },
+    visual_reference:{
+      "3.1 - Part":"../assets/defaultImg.png",
+      "3.2 - Part":"../assets/defaultImg.png",
+      "3.3 - Part":"../assets/defaultImg.png",
+    },
+    specificationData:[
+      {
+        density:"-",
+        packagingStrategy:"-",
+        cartonCode:"-",
+        cartonType:"-",
+        materialType:"-",
+        securemnetClosureType:"-",
+        totalWeight:"-",
+        dimension:["-","-","-"],
+        dunnageDesc:["-","-"],
+        lables:["-","-"],
+        ownership:"-"      
+      },
+      {
+        density:5,
+        packagingStrategy:"Returnable",
+        cartonCode:"NA",
+        cartonType:"Tree Bin Bag",
+        materialType:"Tree Bin Bag",
+        securemnetClosureType:"Remarks",
+        totalWeight:2.19,
+        dimension:[600,400,180],
+        dunnageDesc:["Yes","Xyz"],
+        lables:[2,"Adjacent Side"],
+        ownership:"XYZ"      
+      }
+    ],
+    shippingLogisticData:{
+      vehicleTypeSize:"Closed XYZ",
+      dimension:["NA","NA","NA"],
+      transitMode:"NA",
+      pryPacks:"NA",
+      terms:"XY",
+      materialClass:"NA",
+      loopSize:"5 Days",
+      lifeOfPackaging:"2 Years",
+      loadUnloadResp:"ABCD",
+      supplyAt:"Batch Lot",
+      remarks:"Remarks",
+      percentageUtiize:"76%"
+    },
+    approvalData:[
         {person:"Sanket Pawar",date:"23/02/2024"},
         {person:"Sanket Pawar",date:"23/02/2024"},
       ]
     }
+
+    isArray(obj : any ): obj is any[]{
+      return Array.isArray(obj)
+   }
+
+   entries(obj: any) {
+    return Object.entries(obj);
+  }
   }
 
 
